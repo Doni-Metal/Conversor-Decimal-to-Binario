02c4bc8e43b1e0e7e3a565254216a2d2f0f25018
Doni-Metal <bakury94.ldtc@gmail.com>
2022-09-20 19:31:15 -0500

    Binario a decimal y Suma Agragados

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
 const decInput = document.querySelector('#decimal-input');
-const btnDec = document.querySelector('#btnDec')
-const resultP = document.querySelector('#result')
+const btnDec = document.querySelector('#btnDec');
+const decToBinP = document.querySelector('#decToBinP');
+const binInput = document.querySelector('#binario-input');
+const btnBin = document.querySelector('#btnBin');
+const binToDecP = document.querySelector('#binToDecP');
+const sumAInput = document.querySelector('#sumaA-input');
+const sumBInput = document.querySelector('#sumaB-input');
+const btnSum = document.querySelector('#btnSum');
+const sumBinP = document.querySelector('#sumBinP');
 
 btnDec.onclick = () => {
-  value = Number(decInput.value);
-  let result = binConst(value);
-  resultP.innerText = `Binario: ${result}`
+  let value = Number(decInput.value);
+  let result = decToBin(value);
+  decToBinP.innerText = `Binario: ${result}`
 };
 
-function binConst(num) {
+btnBin.onclick = () => {
+  let value = binInput.value;
+  let numArr = value.split("").map((num) => {
+    return Number(num);
+  })
+  if (!isBin(numArr)) {
+    binToDecP.innerText = `Intenta con un numero Binario (Solo 1's y 0's)`
+  } else {
+    let result = binToDec(numArr);
+    binToDecP.innerText = `Decimal: ${result}`
+  }
+};
+
+btnSum.onclick = () => {
+  let valueA = sumAInput.value;
+  let valueB = sumBInput.value;
+  let arrA = valueA.split('').map((num) => {
+    return Number(num);
+  })
+  let arrB = valueB.split('').map((num) => {
+    return Number(num);
+  })
+  if (!isBin(arrA) || !isBin(arrB)) {
+    sumBinP.innerText = `Intenta con numeros Binarios (Solo 1's y 0's)`;
+  } else {
+    let result = sumBinarios(arrA, arrB);
+    sumBinP.innerText = `Resultado: ${result}`;
+  }
+}
+
+function isBin(elem) {
+  let valid;
+  elem.forEach((num) => {
+    if (num > 1) {
+      valid = false;
+    } else {
+      valid = true  
+    }
+  })
+  return valid
+}
+
+function decToBin(num) {
   let lastBin = 1;
   while (lastBin < num) {
     lastBin *= 2;
@@ -17,6 +66,7 @@ function binConst(num) {
       break
     }
   }
+
   let sum = 0;
   let binaries = [];
   while (lastBin >= 1) {
@@ -29,6 +79,54 @@ function binConst(num) {
     }
     lastBin /= 2;
   }
+
   let result = binaries.join('');
   return result
+}
+
+function binToDec(num) {
+  let decNums = num;
+  exp = decNums.length - 1;
+  let sum = 0;
+  decNums.forEach((num) => {
+    sum += num * (2**exp);
+    exp --;
+  })
+  return sum;
+}
+
+function sumBinarios(a, b) {
+  let numA = a, numB = b;
+  let sizeA = numA.length, sizeB = numB.length
+  if (sizeA > sizeB) {
+    numB.unshift(0);
+  } else if (sizeB > sizeA) {
+    numA.unshift(0);
+  }
+  let result = [];
+  let carry = 0;
+  for (i = sizeA - 1; i >= 0; i--) {
+    console.log(i)
+    debugger
+    let nA = numA[i];
+    let nB = numB[i];
+    if (carry === 1 && nA === 1 && nB === 1) {
+      result.unshift(1);
+      carry --;
+    } else if (nA === 1 && nB === 1) {
+      result.unshift(0);
+      carry ++;
+    } else if (carry === 1) {
+      result.unshift((nA + nB) + carry);
+      carry --;
+    } else {
+      result.unshift(nA + nB);
+    }
+  }
+  if (carry == 1) {
+    result.unshift(carry);
+    carry --;
+  }
+  let final = result.join('');
+  return final;
 }
